8443fd784f89895bd7b3755d72ee86b2c01f9803
Dirk Breeuwer <departdisclose@protonmail.com>
2023-04-23 13:30:28 +0000

    Refactor: Create a class for better organization and encapsulation

--- scraper.py
+++ scraper.py
@@ -2,79 +2,84 @@ import requests
 import json
 import time
 
-
-def get_events(page_number, url, headers, payload):
-    payload["variables"]["page"] = page_number
-    response = requests.post(url, headers=headers, json=payload)
-    data = response.json()
-
-    if 'data' not in data:
-        print(f"Error: {data}")
-        return []
-
-    return data["data"]["eventListings"]["data"]
-
-
-
-def print_event_details(events):
-    for event in events:
-        event_data = event["event"]
-        print(f"Event name: {event_data['title']}")
-        print(f"Date: {event_data['date']}")
-        print(f"Start Time: {event_data['startTime']}")
-        print(f"End Time: {event_data['endTime']}")
-        print(f"Artists: {[artist['name'] for artist in event_data['artists']]}")
-        print(f"Venue: {event_data['venue']['name']}")
-        print(f"Event URL: {event_data['contentUrl']}")
-        print(f"Number of guests attending: {event_data['attending']}")
-        print("-" * 80)
-
-
-def generate_payload(areas, listing_date_gte, listing_date_lte):
-    with open("graphql_query_template.json", "r") as file:
-        payload = json.load(file)
-
-    # Update the relevant fields in the payload
-    payload["variables"]["filters"]["areas"]["eq"] = areas
-    payload["variables"]["filters"]["listingDate"]["gte"] = listing_date_gte
-    payload["variables"]["filters"]["listingDate"]["lte"] = listing_date_lte
-
-    return payload
-
-
-
-
-url = 'https://ra.co/graphql'
-headers = {
+URL = 'https://ra.co/graphql'
+HEADERS = {
     'Content-Type': 'application/json',
     'Referer': 'https://ra.co/events/uk/london',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:106.0) Gecko/20100101 Firefox/106.0'
 }
-
-areas = 13
-listing_date_gte = "2023-04-23T00:00:00.000Z"
-listing_date_lte = "2023-04-29T00:00:00.000Z"
-payload = generate_payload(areas, listing_date_gte, listing_date_lte)
+QUERY_TEMPLATE_PATH = "graphql_query_template.json"
+DELAY = 10  # Adjust this value as needed
 
 
-# Initialize the page number
-page_number = 1
+class EventFetcher:
+    def __init__(self, areas, listing_date_gte, listing_date_lte):
+        self.payload = self.generate_payload(areas, listing_date_gte, listing_date_lte)
 
-# Define the delay between requests (in seconds)
-delay = 10  # Adjust this value as needed
+    @staticmethod
+    def generate_payload(areas, listing_date_gte, listing_date_lte):
+        with open(QUERY_TEMPLATE_PATH, "r") as file:
+            payload = json.load(file)
 
-# Loop until there are no more events left
-while True:
-    events = get_events(page_number, url, headers, payload)
+        payload["variables"]["filters"]["areas"]["eq"] = areas
+        payload["variables"]["filters"]["listingDate"]["gte"] = listing_date_gte
+        payload["variables"]["filters"]["listingDate"]["lte"] = listing_date_lte
 
-    # Break the loop if there are no more events
-    if not events:
-        break
+        return payload
 
-    print_event_details(events)
+    def get_events(self, page_number):
+        self.payload["variables"]["page"] = page_number
+        response = requests.post(URL, headers=HEADERS, json=self.payload)
 
-    # Increment the page number
-    page_number += 1
+        try:
+            response.raise_for_status()
+            data = response.json()
+        except (requests.exceptions.RequestException, ValueError):
+            print(f"Error: {response.status_code}")
+            return []
 
-    # Sleep for the specified delay before sending the next request
-    time.sleep(delay)
+        if 'data' not in data:
+            print(f"Error: {data}")
+            return []
+
+        return data["data"]["eventListings"]["data"]
+
+    @staticmethod
+    def print_event_details(events):
+        for event in events:
+            event_data = event["event"]
+            print(f"Event name: {event_data['title']}")
+            print(f"Date: {event_data['date']}")
+            print(f"Start Time: {event_data['startTime']}")
+            print(f"End Time: {event_data['endTime']}")
+            print(f"Artists: {[artist['name'] for artist in event_data['artists']]}")
+            print(f"Venue: {event_data['venue']['name']}")
+            print(f"Event URL: {event_data['contentUrl']}")
+            print(f"Number of guests attending: {event_data['attending']}")
+            print("-" * 80)
+
+    def fetch_and_print_all_events(self):
+        page_number = 1
+
+        while True:
+            events = self.get_events(page_number)
+
+            if not events:
+                break
+
+            self.print_event_details(events)
+            page_number += 1
+            time.sleep(DELAY)
+
+
+def main():
+    areas = 13
+    listing_date_gte = "2023-04-23T00:00:00.000Z"
+    listing_date_lte = "2023-04-29T00:00:00.000Z"
+
+    event_fetcher = EventFetcher(areas, listing_date_gte, listing_date_lte)
+    event_fetcher.fetch_and_print_all_events()
+
+
+if __name__ == "__main__":
+    main()
